c27e400b4c1022321c4c691e5e9f6e901f2b4a52
tarungatla <tarungatla75@gmail.com>
2024-10-12 20:18:16 +0530

    feat: super admin routes done

---
+++ backend/models/commissionSchema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+// For approved commissions
+const commissionSchema = new mongoose.Schema({
+  amount: Number,  
+  user: mongoose.Schema.Types.ObjectId,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Commission = mongoose.model("Commission", commissionSchema);
